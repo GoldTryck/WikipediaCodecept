e45c87c4b2dfefedfb4a0547b187f3404f2bdf8b
Hector Larios <lariosh69@gmail.com>
2025-04-30 02:43:31 -0600

    [fix] Arregla selectores de paginas por idioma y lógica de validación

--- steps/wikipedia_steps.js
+++ steps/wikipedia_steps.js
@@ -3,22 +3,22 @@ const Wiki_main_page = require('../pages/Wiki_main_page');
 const context = require('../utils/context');
 
 Given('Estoy en la pagina principal de Wikipedia', async () => {
-    Wiki_main_page.open();
+    await Wiki_main_page.open();
 });
 
 When('Selecciono {string} como idioma', async (idioma) => {
-    Wiki_main_page.selectLanguage(idioma);
+    await Wiki_main_page.selectLanguage(idioma);
 });
 When('Ingreso {string} en el campo de búsqueda', async (texto) => {
-    Wiki_main_page.fillSearchField(texto);
+    await Wiki_main_page.fillSearchField(texto);
 });
 
 Then('Se muestra la página de resultados relacionada con {string}', async (texto) => {
-    Wiki_main_page.validateSearchResults(texto);
+    await Wiki_main_page.validateSearchResults(texto);
 });
 
 Then("No se encontraron resultados relacionados", async () => {
-    Wiki_main_page.validateNoResults();
+    await Wiki_main_page.validateNoResults();
 });
 
 Then('Se muestra el mensaje de bienvenida correspondiente en {string}', async (idioma) => {
